e9c919dedc062801bd698e7be33f2419854ace75
jessfraser8 <jfraser4@usf.edu>
2024-06-21 19:49:29 -0400

    Render bars and group elements based on scaled data.

--- script.js
+++ script.js
@@ -5,22 +5,40 @@ function main() {
     var data = [100, 420, 230, 850, 560, 925]
     const margin = 1
     const barHeight = 20
+    var height = data.length * barHeight + 20
     var width = 500
-    var height = data.length * barHeight + data.length * margin
 
     // Create SVG element.
     var svg = d3.select('body').append('svg')
         .attr("width", width)
         .attr("height", height)
 
-    // Create X-axis.
+    // X-scale
     var xScale = d3.scaleLinear()
         .domain([d3.min(data), d3.max(data)])
         .range([50, width])
+    
+    // Make the bars.
+    var bar = svg.selectAll('g')
+        .data(data)
+        .enter().append('g')
+        .attr('transform', function (d, i) {
+            return 'translate(0,'+ i * barHeight +')'
+        });
 
+    // Add bars.
+    bar.append("rect").attr('width', function(d){
+        return xScale(d);
+    })
+        .attr('height', barHeight - 1)
+    
+    // Add x-axis 
     var x_axis = d3.axisBottom()
         .scale(xScale);
 
     svg.append('g')
+        .attr("transform", `translate(0, ${height -20})`)
         .call(x_axis)
+
+
 }
